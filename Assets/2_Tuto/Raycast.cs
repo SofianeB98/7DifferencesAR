67655b28be5cac2576df51b373345a3f1f4ba2cc
Sofiane <sofiane.benembareck@hotmail.com>
2020-01-11 15:26:35 +0100

    divers test marker target + ground + raycast

---
+++ Assets/2_Tuto/Raycast.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 50.0f))
+        {
+            if(hit.collider != null)
+                hit.transform.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
