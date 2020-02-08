2e8eab0adeb8fcfab4c1e258551645bd9e75dde9
Benoît <benoitremy74@gmail.com>
2020-02-08 23:05:53 +0100

    Can set custom value

--- Assets/Game/Scripts/PlayerScripts/GameMode.cs
+++ Assets/Game/Scripts/PlayerScripts/GameMode.cs
@@ -14,9 +14,30 @@ public class GameMode : MonoBehaviour
     [SerializeField] private Sprite buttonEnableBackground;
     [SerializeField] private Sprite buttonDisableBackground;
 
+    // Slider UI
+    [SerializeField] private Slider timerSlider;
+    [SerializeField] private Slider lifeSlider;
+
+    // Slider Text
+    [SerializeField] private Text timerText;
+    [SerializeField] private Text lifeText;
+
+    // Game Mode Settings
+    public bool freeModeEnable;
+    public bool lifeModeEnable;
+    public bool timerModeEnable;
+    public int defaultLife;
+    public int defaultTimer;
+
     // Start is called before the first frame update
     void Start()
     {
+        timerSlider.value = GameModeSettings.defaultTimer;
+        lifeSlider.value = GameModeSettings.defaultLife;
+
+        SetTextTimer(GameModeSettings.defaultTimer);
+        SetTextLife(GameModeSettings.defaultLife);
+
         if (GameModeSettings.freeModeEnable)
         {
             buttonFreeMode.GetComponent<Image>().sprite = buttonEnableBackground;
@@ -35,6 +56,16 @@ public class GameMode : MonoBehaviour
             buttonLifeMode.GetComponent<Image>().sprite = buttonDisableBackground;
             buttonTimerMode.GetComponent<Image>().sprite = buttonEnableBackground;
         }
+    }
+
+    private void Update()
+    {
+        // Show Game Mode Settings in Unity inspector
+        freeModeEnable = GameModeSettings.freeModeEnable;
+        lifeModeEnable = GameModeSettings.lifeModeEnable;
+        timerModeEnable = GameModeSettings.timerModeEnable;
+        defaultLife = GameModeSettings.defaultLife;
+        defaultTimer = GameModeSettings.defaultTimer;
     }
 
     public void ActivateFreeMode()
@@ -68,5 +99,25 @@ public class GameMode : MonoBehaviour
         buttonFreeMode.GetComponent<Image>().sprite = buttonDisableBackground;
         buttonLifeMode.GetComponent<Image>().sprite = buttonDisableBackground;
         buttonTimerMode.GetComponent<Image>().sprite = buttonEnableBackground;
+    }
+
+    public void SetTimer(float timer)
+    {
+        GameModeSettings.defaultTimer = (int)timer;
+    }
+
+    public void SetTextTimer(float timer)
+    {
+        timerText.text = timer.ToString() + " secondes";
+    }
+
+    public void SetLife(float life)
+    {
+        GameModeSettings.defaultLife = (int)life;
+    }
+
+    public void SetTextLife(float life)
+    {
+        lifeText.text = "Nombre de vie : " + life.ToString();
     }
 }
